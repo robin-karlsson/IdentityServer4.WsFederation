3e6a2e1fd5b81e7a49fed0c6c155fc57eb9f25d7
Robin Karlsson <robin.karlsson@teleopti.com>
2019-04-16 13:30:28 +0200

    Adding first test case for ws-federation endpoint

--- IdentityServer4.WsFederation.Tests/Endpoints/WsFederationSigninEndpointTests.cs
+++ IdentityServer4.WsFederation.Tests/Endpoints/WsFederationSigninEndpointTests.cs
@@ -1,6 +1,10 @@
-﻿using IdentityServer4.Configuration;
+﻿using System.Net;
+using System.Threading.Tasks;
+using IdentityServer4.Configuration;
+using IdentityServer4.Endpoints.Results;
 using IdentityServer4.Services;
 using IdentityServer4.WsFederation.Validation;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
@@ -10,16 +14,21 @@ namespace IdentityServer4.WsFederation.Tests.Endpoints
     [TestClass]
     public class WsFederationSigninEndpointTests
     {
-        private ILogger _logger = Substitute.For<ILogger<WsFederationEndpoint>>();
+        private readonly ILogger<WsFederationEndpoint> _logger = Substitute.For<ILogger<WsFederationEndpoint>>();
         private IdentityServerOptions _options;
         private IWsFederationRequestValidator _validator;
         private IWsFederationResponseGenerator _responseGenerator;
         private IUserSession _userSession;
 
         [TestMethod]
-        public void PostShouldReturnInvalidMethod()
+        public async Task PostShouldReturnInvalidMethod()
         {
+            var endpoint = new WsFederationEndpoint(_logger, _validator, _responseGenerator, _userSession);
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Method = "POST";
+            var result = (StatusCodeResult)await endpoint.ProcessAsync(httpContext);
 
+            Assert.AreEqual((int)HttpStatusCode.MethodNotAllowed, result.StatusCode);
         }
     }
 }
